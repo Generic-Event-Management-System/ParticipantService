591026dd709f2a99646535786238bffe95c8bb17
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 19:48:58 +0200

    Registered automapper in the program.cs file

--- ParticipantService/Program.cs
+++ ParticipantService/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ParticipantDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(typeof(Program));
+
 // Add services to the container.
 builder.Services.AddScoped<IParticipantsService, ParticipantsService>();
 
