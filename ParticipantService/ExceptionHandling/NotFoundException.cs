915a1b7184428420fb84c24a32f869757871def4
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 19:20:36 +0200

    Added custom NotFoundException class for customized exception response from the API.

---
+++ ParticipantService/ExceptionHandling/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace ParticipantService.ExceptionHandling
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
