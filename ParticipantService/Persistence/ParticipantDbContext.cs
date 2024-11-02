84f3730f78a26c9ccf447fda071a8143f363ad3a
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 19:09:37 +0200

    Added DbContext in the project and added the necessary nuget packages also registered the DbContext in the program.cs file.

---
+++ ParticipantService/Persistence/ParticipantDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParticipantService.Models.Entities;
+
+namespace ParticipantService.Persistence
+{
+    public class ParticipantDbContext : DbContext
+    {
+        public DbSet<Participant> Participants { get; set; }
+
+        public ParticipantDbContext(DbContextOptions<ParticipantDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
