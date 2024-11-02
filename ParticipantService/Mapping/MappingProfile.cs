218cceead727617658c12ed58a09488ad6b9cde6
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 19:14:29 +0200

    Added automapper extension in the project and also created mapping profile class to map the participant and participant dto entities.

---
+++ ParticipantService/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ParticipantService.Models.Dto;
+using ParticipantService.Models.Entities;
+
+namespace ParticipantService.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Participant, ParticipantDto>();
+            CreateMap<ParticipantDto, Participant>();
+        }
+    }
+}
