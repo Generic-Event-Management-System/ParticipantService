c0b2d77c750db0bcd15bf2e60009a2dc9363c7ba
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-02 19:13:46 +0200

    Created Participant Dto class.

---
+++ ParticipantService/Models/Dto/ParticipantDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ParticipantService.Models.Dto
+{
+    public class ParticipantDto
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required bool IsActive { get; set; } = false;
+    }
+}
